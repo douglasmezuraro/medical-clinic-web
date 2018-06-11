3191464ecc1aab27d914d69e428a0858726841d9
Douglas Mezuraro <douglasmez@gmail.com>
2018-06-10 23:27:58 -0300

    Refatorado controller

--- MedicalClinicWeb/src/java/br/uem/din/medicalclinic/controller/SecretaryController.java
+++ MedicalClinicWeb/src/java/br/uem/din/medicalclinic/controller/SecretaryController.java
@@ -1,15 +1,70 @@
-package br.uem.din.medicalclinic.controller;
+package br.uem.din.medicalclinic.bean;
 
+import br.uem.din.medicalclinic.controller.SecretaryController;
 import br.uem.din.medicalclinic.model.Secretary;
+import java.io.Serializable;
+import java.util.List;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
 
-public class SecretaryController extends BaseController<Secretary> {
+@Named(value = "secretaryBean")
+@SessionScoped
+public class SecretaryBean implements Serializable, Cadastrable<Secretary> {
+
+    private Secretary secretary;
     
-    private static SecretaryController instance;
+    public SecretaryBean() {
+        secretary = new Secretary();
+    }        
+   
+    public Secretary getSecretary() {
+        return secretary;
+    }
     
-    public static SecretaryController getInstance() {
-        if(instance == null)
-            instance = new SecretaryController();
-        return instance;
+    @Override
+    public String add() {
+        SecretaryController.getInstance().add(secretary);
+        return "index";
+    }
+    
+    @Override
+    public List<Secretary> listAll() {
+        return SecretaryController.getInstance().getList();
+    }
+    
+    @Override
+    public String create() {
+        secretary = new Secretary();
+        return "create";
+    }
+    
+    @Override
+    public String index() {
+        return "index";
+    }
+    
+    @Override
+    public String edit(Secretary object) {
+        secretary = object;
+        return "edit";
+    }
+    
+    @Override
+    public String edit() {
+        
+        return "index";
+    }
+    
+    @Override
+    public String delete(Secretary object) {
+        SecretaryController.getInstance().delete(object);
+        return "index";
+    }
+    
+    @Override
+    public String details(Secretary object) {
+        secretary = object;
+        return "details";
     }
     
 }
